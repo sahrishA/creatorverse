da3eeaa680196a549c9587e410b51bbd71ac37eb
sahrishA <159168795+sahrishA@users.noreply.github.com>
2024-08-26 02:08:54 -0400

    Update client.js with the api key url
    API key url was wrog. So I updated it. it's working now

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 const URL = 'https://omqrqnqjmllbbrpzsarq.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tcXJxbnFqbWxsYmJycHpzYXJxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNDYxNDM1NiwiZXhwIjoyMDQwMTkwMzU2fQ._A3CeqOqeVFWBwzE_I116RJ4wjxzbYeqyUCqZChQZ6A';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tcXJxbnFqbWxsYmJycHpzYXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ2MTQzNTYsImV4cCI6MjA0MDE5MDM1Nn0._zK5xYWAikKC_W5dPvmp8Bfn1gF1rhK0nBEJlJc5gfg';
 //const supabase = createClient(URL, API_KEY);
 export const supabase = createClient(URL, API_KEY);
